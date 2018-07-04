aa3f1e7e1c5d3dca35bfde029fbc73fd437232c7
James Lefty Larimer <lefty1044@gmail.com>
2018-07-04 07:37:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.ToLowerCase();
 }
-function shout(string) {
+function logShout(string) {
   console.log(string.toUpperCase());
 }
